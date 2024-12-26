c41ec666ed3449199f3320cd7e25df11f96bd880
JiyuLei <jerrylei.ui@gmail.com>
2024-12-25 18:04:24 -0800

    Jiyu | Create UserList component and page

---
+++ src/components/UserList.tsx
@@ -0,0 +1,5 @@
+export default function UserList() {
+    return <div>
+        this is the User List component
+    </div>
+}
---
+++ src/pages/users.tsx
@@ -0,0 +1,8 @@
+import UserList from "@/components/UserList";
+
+export default function Users() {
+    return <div>
+        this is the user page!
+        <UserList />
+    </div>
+}
