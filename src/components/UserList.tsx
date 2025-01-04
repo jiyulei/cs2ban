39e25452fd2037e537b016d637af5f15e36fa97f
JiyuLei <jerrylei.ui@gmail.com>
2025-01-03 17:38:57 -0800

    Jiyu | Create BannedPlayersTable component

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,3 @@
 export default function UserList() {
-    return <div>
-        this is the User List component
-    </div>
+  return <div>this is the User List component</div>;
 }
--- src/pages/admin/[steamid].tsx
+++ src/pages/admin/[steamid].tsx
@@ -4,6 +4,7 @@ import { fetchRecentGames } from "../../utils/fetchLatestGame";
 import { fetchBannedPlayers } from "../../utils/fetchBannedPlayers";
 import { fetchLeetifyGames } from "../../utils/fetchLeetifyGames";
 import { ADMINS_STEAMID } from "../../utils/constants";
+import BannedPlayerTable from "../../components/BannedPlayerTable";
 
 const AdminDetailsPage = () => {
   const router = useRouter();
@@ -224,9 +225,20 @@ const AdminDetailsPage = () => {
               }`}
               onClick={() => setActiveTab("bannedTeammates")}
             >
-              Banned Players
+              Banned Teammates
             </button>
           </nav>
+
+          <button
+            className={`text-sm font-medium px-4 py-2 border-b-2 ${
+              activeTab === "bannedEnemies"
+                ? "border-blue-500 text-blue-600"
+                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+            }`}
+            onClick={() => setActiveTab("bannedEnemies")}
+          >
+            Banned Enemies
+          </button>
         </div>
 
         {activeTab === "recentGames" && newGames && newGames.length > 0 && (
@@ -255,7 +267,7 @@ const AdminDetailsPage = () => {
                 <tr key={index}>
                   <td className="px-6 py-4">{game.mapName}</td>
                   <td className="px-6 py-4">{game.matchResult}</td>
-                  <td className="px-6 py-4">
+                  <td className="px-3 py-4">
                     {game.kills} / {game.deaths}
                   </td>
                   <td className="px-6 py-4">
@@ -295,30 +307,11 @@ const AdminDetailsPage = () => {
         )}
 
         {activeTab === "bannedTeammates" && bannedTeammates.length > 0 && (
-          <table className="min-w-full divide-y divide-gray-200 bg-white shadow-md rounded-lg mt-6">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
-                  Player Name
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
-                  Ban Reason
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
-                  Times Banned
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {bannedTeammates.map((player, index) => (
-                <tr key={index}>
-                  <td className="px-6 py-4">{player.playerName}</td>
-                  <td className="px-6 py-4">{player.banReason}</td>
-                  <td className="px-6 py-4">{player.timesBanned}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <BannedPlayerTable />
+        )}
+
+        {activeTab === "bannedEnemies" && bannedEnemies.length > 0 && (
+          <BannedPlayerTable />
         )}
       </div>
     </div>
