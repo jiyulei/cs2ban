3c8aed29c5d23128001e6cafe869374442d1d4ee
JiyuLei <jerrylei.ui@gmail.com>
2025-01-05 05:33:06 -0800

    Jiyu | Add type for Tables

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type Teammate = string;
+
+export type Teammates = {
+  gameId: string;
+  teammates: Teammate[];
+  date: string;
+};
+
+export type Enemy = string;
+
+export type Enemies = {
+  gameId: string;
+  enemies: Enemy[];
+  date: string;
+};
